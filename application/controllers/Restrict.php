2ea59a6e230974c2ab8c436ba36b26de436e7b59
Giovane Verbinnen <giovane.oliveira@funcionalconsultoria.com.br>
2021-05-18 11:55:13 -0300

    initial commit

---
+++ application/controllers/Restrict.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Restrict extends CI_Controller {
+
+    public function index()
+	{
+		$this->load->model("users_model");
+		print_r($this->users_model->get_user_data("admin"));
+		//$this->template->show('login.php');
+	}
+}
---
+++ application/views/restrict.php
@@ -0,0 +1,11 @@
+<section id="about" class="light-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="section-title">
+                    <h2>Login</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
