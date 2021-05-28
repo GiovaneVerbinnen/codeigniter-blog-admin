44819852430d2c918643a05a75378abf3026eff7
Giovane Verbinnen <giovane.oliveira@funcionalconsultoria.com.br>
2021-05-28 13:48:51 -0300

    aula.36 - começando area restrita

--- application/views/login.php
+++ application/views/login.php
@@ -1,51 +1,51 @@
+<section style="min-height: calc(100vh - 83px)" class="light-bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-offset-3 col-lg-6 text-center">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<div class="section-title">
+							<h2>LOGIN</h2>
+						</div>
+						<form id="login_form" method="post">
+							
+									<div class="form-group">
+										<div class="input-group">
+											<div class="input-group-addon">
+												<span class="glyphicon glyphicon-user"></span>
+											</div>
+											<input type="text" placeholder="Usuário" id="username" name="username"
+											class="form-control">
+										</div>
+										<span class="help-block"></span>
+									</div>
+							
 
-<section style="min-height:calc(100vh - 83px);" class="light-bg">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-offset-3 col-lg-6 text-center">
-                <div class="section-title">
-                    <h2>Login</h2>
-                    <form action="login_form">
-                        <div class="row">
-                            <div class="column">
-                                <div class="co-lg-12">
-                                    <div class="form-group">
-                                    <div class="input-group">
-                                    <div class="input-group-addon">
-                                    <span class="glyphicon glyphicon-user"></span>
-                                    </div>
-                                    <input type="text" placeholder="Usuário" name="username" class="form-control">
-                                    </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                       
+							
+									<div class="form-group">
+										<div class="input-group">
 
-                        <div class="row">
-                            <div class="co-lg-12">
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">
-                                            <span class="glyphicon glyphicon-lock"></span>
-                                        </div>
-                                        <input type="password" placeholder="Senha" name="submit_password" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="co-lg-12">
-                                <div class="form-group">
-                                    <button type="submit" id="btn-login" class="btn btn-block">Entrar</button>
-                                </div>
-                                <span class="help-block"></span>
-                            </div>
-                        </div>
+											<div class="input-group-addon">
+												<span class="glyphicon glyphicon-lock"></span>
+											</div>
 
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+											<input type="password" placeholder="Senha" name="password"
+											class="form-control">
+										</div>
+									</div>
+							
+									<div class="form-group">
+										<button type="submit" id="btn_login" class="btn btn-block">Login</button>
+									</div>
+									<span class="help-block"></span>
+							
+
+						</form> 
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<!-- /.container -->
 </section>
