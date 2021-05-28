8f943535100d3429f6d4310e6a4e03e7062ddf5e
Giovane Verbinnen <giovane.oliveira@funcionalconsultoria.com.br>
2021-05-28 13:41:27 -0300

    aula.36 - começando area restrita

--- application/views/restrict.php
+++ application/views/restrict.php
@@ -1,11 +1,26 @@
-<section id="about" class="light-bg">
+<section style="min-height: calc(100vh - 83px)" class="light-bg">
     <div class="container">
         <div class="row">
-            <div class="col-lg-12 text-center">
+            <div class="col-lg-offset-3 col-lg-6 text-center">
                 <div class="section-title">
-                    <h2>Login</h2>
+                    <h2>area restrita</h2>
+                </div>
+                <p>.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-offset-5 col-lg-2 text-center">
+                <div class="form-group">
+                    <a class="btn btn-link" href="#"><i class="fa fa-user"></i></a>
+                    <a class="btn btn-link" href="restrict/logoff"><i class="fa fa-sign-out"></i> </a>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container">
+        <ul class="nav nav-tabs">
+            <li></li>
+        </ul>
+        
     </div>
 </section>
